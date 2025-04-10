21c58a2b4fb91b5a3d54efcd4d79debac3f0248c
田口　幹太 <kantataguchi@kanta2024pc.local>
2025-04-10 17:36:33 +0900

    initial commit

---
+++ src/test/java/com/moattravel/moattravel/MoattravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moattravel.moattravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoattravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
