71da9b25102c40d7d246cf8950d88570f3ee8932
kanta170434 <kanta170434@gmail.com>
2025-04-11 16:06:31 +0900

    0411コミット

---
+++ src/main/java/com/moattravel/moattravel/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.moattravel.moattravel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
